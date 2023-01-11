23d0f82f1cf034f16ac15dbaa86a7c80d79372dd
itsnichi4 <120479794+itsnichi4@users.noreply.github.com>
2023-01-11 20:14:25 +0200

    Add firstNum and secondNum variables Create for loop in order to create fibonacci sequence Add return to show the number Add if condition to return number or return 'OOPS' if the entered number is negative

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    let firstNum = 0
+    let secondNum = 1
+
+    nextNum = firstNum + secondNum
+    if (num > 0) {
+        for (i = 0; i < num - 2; i++) {// because the first and second numbers are already written (firstNum, secondNum)
+            firstNum = secondNum
+            secondNum = nextNum
+            nextNum = firstNum + secondNum
+
+        }
+        return nextNum
+    }
+    else return "OOPS"
 
 };
-
+console.log(fibonacci)
 // Do not edit below this line
 module.exports = fibonacci;
