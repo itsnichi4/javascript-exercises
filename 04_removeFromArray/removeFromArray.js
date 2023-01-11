8356cb731d6a18e10ed4249105a4a2fea185d0d0
itsnichi4 <120479794+itsnichi4@users.noreply.github.com>
2023-01-11 13:58:35 +0200

    Added all files to be watched

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
 
-};
 
+ function {console.log(array,...args)}
+
+    
+        return array.filter(item => {console.log(item)})
+    };
+    
+    
+    
+console.log(removeFromArray([1, 2, 3, 4], 3, 2))
 // Do not edit below this line
 module.exports = removeFromArray;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+
+let y = Math.round(((x - 32) * 0.5556) * 10)/10;
+return y;
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+
+  let y = Math.round((x * 1.8 + 32)*10) / 10;
+return y
+
 };
+
+
+
 
 // Do not edit below this line
 module.exports = {
