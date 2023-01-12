17b173dad24c702b5a22edd9389c16710735b6ce
itsnichi4 <120479794+itsnichi4@users.noreply.github.com>
2023-01-12 20:52:09 +0200

    Add filter array to calculate the age of each person Add if condition to check for .yearOfDeath exists Return the age = substract the year of birth from current year Return the age = substract the year of birth from the year of death Add maxAge variable to check for max age Add index to get index of max age Return value of person from the same index

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+    let filter = arr.map(people => {
+        if (people.yearOfDeath === undefined) {
+            return 2023 - people.yearOfBirth;
+        }
+        else {
+            return people.yearOfDeath - people.yearOfBirth;
+        }
+    });
 
+    let maxAge = Math.max(...filter);
+    let index = filter.indexOf(maxAge);
+    return arr[index];
 };
 
 // Do not edit below this line
