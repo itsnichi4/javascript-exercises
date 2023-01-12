398682ef6a55d4881bc4d427afd02d8fe3f66a1c
itsnichi4 <120479794+itsnichi4@users.noreply.github.com>
2023-01-12 16:06:51 +0200

    Add variable Add function to return only the nested object 'title'

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function (arr) {
+    let filtered = arr.map(books => (books.title))
+    return filtered
 
-};
+}
+
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
