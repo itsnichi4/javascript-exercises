bb3fabdd778cd8b10b03581010bbfd29e3e81440
itsnichi4 <120479794+itsnichi4@users.noreply.github.com>
2023-01-16 18:31:25 +0200

    Rework if statement to start with the short circuit Remove console.log

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -3,18 +3,17 @@ const fibonacci = function (num) {
     let secondNum = 1
 
     nextNum = firstNum + secondNum
-    if (num > 0) {
+    if (num <= 0) return "OOPS";
+    else {
         for (i = 0; i < num - 2; i++) {// because the first and second numbers are already written (firstNum, secondNum)
             firstNum = secondNum
             secondNum = nextNum
             nextNum = firstNum + secondNum
 
         }
-        return nextNum
+        return nextNum;
     }
-    else return "OOPS"
 
 };
-console.log(fibonacci)
 // Do not edit below this line
 module.exports = fibonacci;
